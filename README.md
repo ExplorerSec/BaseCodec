8132e8e40533f9d91f817b394876010ae43abdbc
ExplorerSec <gaoweixu@foxmail.com>
2025-05-05 20:05:44 +0800

    0.1.2 支持文件输入

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# BaseCodec 
+# BaseCodec 0.1.2
 
 A highly extensible and customizable Base64 encoder/decoder that allows redefining the encoding character set, specific algorithms for each encoding group, etc. 
 
@@ -6,12 +6,13 @@ A highly extensible and customizable Base64 encoder/decoder that allows redefini
 
 #### **Command Options:** 
 
-- e|-e Encoding mode 
-- d|-d Decoding mode
+- e|-e      Encoding mode 
+- d|-d      Decoding mode
+- fe|-fe    [Read from file] Encoding mode 
+- fd|-fd    [Read from file] Decoding mode
 
 
-
-# BaseCodec
+# BaseCodec 0.1.2
 
 一个高度可扩展可自定义 Base64 编码解码器，允许重新定义编码字符集、每个编码分组的具体算法等。
 
@@ -19,5 +20,7 @@ A highly extensible and customizable Base64 encoder/decoder that allows redefini
 
 #### **命令选项:**
 
-- e|-e 编码模式
-- d|-d 解码模式
+- e|-e      编码模式
+- d|-d      解码模式
+- fe|-fe    [从文件读入]编码模式 
+- fd|-fd    [从文件读入]解码模式
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,15 @@
 mod base64;
 use base64::Base64Codec;
+use std::fs::read_to_string;
 use std::env::args;
 
 fn show_help(){
-    println!("Base64_v0.1.1 [Option] [Text]");
+    println!("Base64_v0.1.2 [Option] [Text]");
     println!("Option:");
     println!("    e|-e      to encode");
     println!("    d|-d      to decode");
+    println!("    fe|-fe    read from file and encode");
+    println!("    fd|-fd    read from file and decode");
 }
 
 fn main() {
@@ -20,6 +23,22 @@ fn main() {
                 },
                 "d"|"-d" =>{
                     println!("{}",ct.decode(&args[2]));
+                },
+                "fe"|"-fe" =>{
+                    if let Ok(s) = read_to_string(&args[2]){
+                        println!("{}",ct.encode(&s));
+                    }
+                    else {
+                        println!("[Error] Failed to read file");
+                    }
+                },
+                "fd"|"-fd" =>{
+                    if let Ok(s) = read_to_string(&args[2]){
+                        println!("{}",ct.decode(&s));
+                    }
+                    else {
+                        println!("[Error] Failed to read file");
+                    }
                 },
                 _ => show_help(),
             }
